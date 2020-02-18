4ab43bd1edf3b5acb6173c864e39733254c3c3b5
jdcia <jacob.garcia@student.nmt.edu>
2020-02-18 18:31:58 +0000

    Created file for summarizer and made the functions and call in main. Just need to implement the functions next.

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,6 +1,7 @@
 
 import input_reader
 import text_speech
+import summarizer
 
 '''
 Main function for Text_Reader. This controls the flow of the program before the frontend is built
@@ -8,14 +9,29 @@ Main function for Text_Reader. This controls the flow of the program before the 
 
 def main():
 
+    #TODO add command line arguements
+
     #create reader object
     reader = input_reader.reader("/home/jacob/Projects/Text_Reader/src/test.pdf")
 
+    text_pdf = reader.analyze()
+
     #convert to text and create speaker object
-    speaker = text_speech.speaker(reader.analyze())
+    speaker = text_speech.speaker(text_pdf)
 
     #play the output
     speaker.speak()
+
+    #summarize the text into a condences version
+
+    #create condencer object
+    condencer = summarizer.condencer(text_pdf)
+
+    #call to summarize the text.
+    summed_text = condencer.summarize()
+
+    
+    print(summed_text)
 
 
 
---
+++ src/backend/summarizer.py
@@ -0,0 +1,26 @@
+
+
+'''
+This call will use nltk library reference for start
+ https://becominghuman.ai/text-summarization-in-5-steps-using-nltk-65b21e352b65
+'''
+
+
+'''
+This class will take in text from a pdf and summarize it to a condenced form.
+'''
+class condencer():
+
+    def __init__(self, text):
+        self.to_sum = text
+
+    def summarize(self):
+
+        #Summarize the text and return the final summarized text.
+
+        summary = ""
+
+
+        return summary
+
+    
