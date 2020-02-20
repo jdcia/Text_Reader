6b2215edfbd2983382e15a593c19967e4b51aa09
jdcia <jacob.garcia@student.nmt.edu>
2020-02-20 17:02:41 +0000

    Started adding command lines args to the program for easy testing.

--- src/backend/main.py
+++ src/backend/main.py
@@ -2,6 +2,7 @@
 import input_reader
 import text_speech
 import summarizer
+import argparse
 
 '''
 Main function for Text_Reader. This controls the flow of the program before the frontend is built
@@ -9,18 +10,33 @@ Main function for Text_Reader. This controls the flow of the program before the 
 
 def main():
 
-    #TODO add command line arguements
+    #create parser object
+    parser = argparse.ArgumentParser()
+    
+    #parser arguments
+    parser.add_argument("-p", "--play", help="This arg will play the mp3 after creation", action="store_true")
+    parser.add_argument("-t", "--text", help="This arg will print the text from the pdf", action="store_true")
+    parser.add_argument("-s", "--sum", help="This arg will print the summary of the pdf", action="store_true")
+    
+    #get arguements
+    args = parser.parse_args()
 
     #create reader object
     reader = input_reader.reader("./test.pdf")
 
+    #get text from the pdf
     text_pdf = reader.analyze()
+
+    if args.text == True:
+        print("Text from PDF:")
+        print(text_pdf)
 
     #convert to text and create speaker object
     speaker = text_speech.speaker(text_pdf)
 
     #play the output
-    #speaker.speak()
+    if args.sum == True:
+        speaker.speak()
 
     #summarize the text into a condences version
 
@@ -30,11 +46,9 @@ def main():
     #call to summarize the text.
     summed_text = condencer.summarize()
 
-
-    print(summed_text)
-
-
-
+    if args.sum == True:
+        print("Summarized Text:")
+        print(summed_text)
 
 
 if __name__ == '__main__':
