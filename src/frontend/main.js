59230de8dc55d1cae93364f804b04768ad0d6b48
jdcia <jacob.garcia@student.nmt.edu>
2020-04-18 16:25:27 +0000

    reinstalling laptop backing up all repostories

--- src/frontend/main.js
+++ src/frontend/main.js
@@ -17,9 +17,7 @@ function createWindow () {
   win.webContents.openDevTools()
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+//creates window
 app.whenReady().then(createWindow)
 
 // Quit when all windows are closed.
@@ -41,3 +39,8 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+function test(){
+    
+    console.log("TEST")
+}
